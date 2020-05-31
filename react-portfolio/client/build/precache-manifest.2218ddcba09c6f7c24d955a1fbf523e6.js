1f4abe25bba8e90a8de712c7a41046445fdcadcd
Ryan Tam <ryanjtam@gmail.com>
2020-05-31 01:27:06 -0700

    built the react app and not pushing to master to make app work

---
+++ react-portfolio/client/build/precache-manifest.2218ddcba09c6f7c24d955a1fbf523e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28c8c69d78ac14ed30e1",
+    "url": "/static/css/main.10fba54e.chunk.css"
+  },
+  {
+    "revision": "28c8c69d78ac14ed30e1",
+    "url": "/static/js/main.3c503450.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f56c199863967b51bc88",
+    "url": "/static/js/2.5ab16818.chunk.js"
+  },
+  {
+    "revision": "474b2f942dcfffe0abbf9331d6555504",
+    "url": "/index.html"
+  }
+];
