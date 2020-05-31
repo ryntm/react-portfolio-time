95832c3c7d6c04c74680202cc2641e6cfbfb2c77
Ryan Tam <ryanjtam@gmail.com>
2020-05-31 03:03:38 -0700

    changed the server file

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-if(process.env.NODE_ENV == 'production'){ app.use(express.static(path.join(__dirname,'client/build')))};
+// if(process.env.NODE_ENV == 'production'){ app.use(express.static(path.join(__dirname,'client/build')))};
 
 // Start the API server
 app.listen(PORT, function() {
