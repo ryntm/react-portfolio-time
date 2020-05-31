01f7b35a4eff42c9d9a39c38b23d05ee3fd0d719
Ryan Tam <ryanjtam@gmail.com>
2020-05-31 02:49:56 -0700

    restructuring the app

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-// app.use(routes);
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+if(process.env.NODE_ENV == 'production'){ app.use(express.static(path.join(__dirname,'client/build')))};
 
 // Start the API server
 app.listen(PORT, function() {
