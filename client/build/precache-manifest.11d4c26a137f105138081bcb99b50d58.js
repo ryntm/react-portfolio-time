ced0dfe790389d8912e7a5431a3dd195485dcdfe
Ryan Tam <ryanjtam@gmail.com>
2020-05-31 17:09:19 -0700

    rebuilding

---
+++ client/build/precache-manifest.11d4c26a137f105138081bcb99b50d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b968b260e3698014082f",
+    "url": "/static/css/main.6a1d5b32.chunk.css"
+  },
+  {
+    "revision": "b968b260e3698014082f",
+    "url": "/static/js/main.3c1a3f4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f56c199863967b51bc88",
+    "url": "/static/js/2.5ab16818.chunk.js"
+  },
+  {
+    "revision": "0ee5798e70e0b1d37b51db65382ad908",
+    "url": "/index.html"
+  }
+];
