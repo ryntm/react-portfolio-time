cdf2391fa6c483ae3428fcea8325224c4d465a6b
Ryan Tam <ryanjtam@gmail.com>
2020-06-10 21:57:48 -0700

    Updates

---
+++ precache-manifest.bc05d49b03223a1c80d4e68b6b1d6f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4936544218c35e6e87d5dd60672f1673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5381ade034409dbc10a",
+    "url": "/static/css/main.c8ee9c89.chunk.css"
+  },
+  {
+    "revision": "81ebd6d8d6ea4dd2609d",
+    "url": "/static/js/2.edc6b2a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.edc6b2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5381ade034409dbc10a",
+    "url": "/static/js/main.44225df6.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  }
+]);
